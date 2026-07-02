b41234a83b52b0b89484cf7a212e0c8f78135dc2
alewebcristo21-dotcom <alewebcristo21@gmail.com>
2026-07-02 23:22:18 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,15 @@
-const CACHE_NAME = 'vivalivreterapias-v1';
+const CACHE_NAME = 'vivalivreterapias-v3';
 const urlsToCache = [
   '/',
   '/index.html',
   '/mentora',
-  '/mentora.html'
+  '/mentora.html',
+  '/guia',
+  '/guia.html'
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
@@ -14,10 +17,9 @@ self.addEventListener('install', event => {
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      if (response) return response;
-      return fetch(event.request).catch(() => caches.match('/index.html'));
-    })
+    fetch(event.request).catch(() => 
+      caches.match(event.request).then(response => response || caches.match('/index.html'))
+    )
   );
 });
 
@@ -25,6 +27,6 @@ self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
       Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
+    ).then(() => self.clients.claim())
   );
 });
